1e2dd06b04e78b2e6012e2e9c3e5010b7f1e50a2
ricardo-sousa <rwmsousa@gmail.com>
2022-03-23 11:21:58 -0300

    exercise resolution

--- src/main/java/com/trybe/contapoupanca/ContaPoupanca.java
+++ src/main/java/com/trybe/contapoupanca/ContaPoupanca.java
@@ -1,5 +1,34 @@
 package com.trybe.contapoupanca;
 
 public class ContaPoupanca {
+  double saldo;
+  String titularConta;
 
+  public ContaPoupanca(String nomeTitular, double depositoInicial) {
+    this.depositar(depositoInicial);
+    this.insereTitularConta(nomeTitular);
+  }
+
+  public void insereTitularConta(String titularConta) {
+    this.titularConta = titularConta;
+  }
+
+  public void depositar(double valor) {
+    this.saldo += valor;
+  }
+
+  public void sacar(double valor) {
+    if (saldo < valor) {
+      System.out.println("Saldo insuficiente");
+    }
+    this.saldo -= valor;
+  }
+
+  public double mostrarSaldo() {
+    return this.saldo;
+  }
+
+  public String mostrarTitularConta() {
+    return this.titularConta;
+  }
 }
